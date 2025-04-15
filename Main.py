17f74625e24452f9254706a49feebc7441dff80e
Volodymyr Zhytelnyi <volodymyr.zhytelnyi@gmail.com>
2025-04-15 23:18:42 +0300

    Add README and update Main

--- Main.py
+++ Main.py
@@ -1,1 +1,17 @@
+from logic import *
 
+# Firstly we need to create our literal
+# Using the example from the README, we need to create 2 literals “Sunny” and “Play”
+A = Literal("Sunny")
+B = Literal("Play")
+
+# After we need to connect them using implication
+implication = Implication(A, B)
+
+# Now we need to add knowledge and negation of goal
+formula = And(Or(Not(A), B), A, Not(B))
+
+# Now we apply resolution method to check if contradiction appears
+# If we get True (an empty clause), it means our conclusion "Play" is logically valid
+resolution = Resolution(formula)
+print(resolution.resolve())
