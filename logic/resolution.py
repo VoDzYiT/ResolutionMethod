07c7617dc824630e77e544c7f4ea4ee48d74d0d4
Volodymyr Zhytelnyi <volodymyr.zhytelnyi@gmail.com>
2025-04-24 20:48:16 +0300

    Refactoring

--- logic/resolution.py
+++ logic/resolution.py
@@ -8,71 +8,48 @@ class Resolution:
         self.formula = formula
 
     def resolve(self):
-        formula = self.formula
-        # print(formula)
         formula = to_cnf(self.formula)
-        # print(formula)
         cnf_set = to_cnf_set(formula)
-        # print(cnf_set)
 
         changed = True
         while changed:
             changed = False
             i = 0
+
             while i < len(cnf_set):
                 j = i + 1
                 while j < len(cnf_set):
                     clause1 = cnf_set[i]
                     clause2 = cnf_set[j]
-                    controversial = resolve(clause1, clause2)
-                    if controversial is not None:
 
-                        lit1, lit2 = controversial
+                    conflicting = resolve(clause1, clause2)
+                    if conflicting is not None:
+                        lit1, lit2 = conflicting
 
-                        if isinstance(lit1, Literal) and len(clause1) == 1:
-                            cnf_set.pop(j)
-                            cnf_set.pop(i)
-                            changed = True
-
-                            break
-
-                        if isinstance(lit1, list):
-                            cnf_set.pop(j)
-                            cnf_set.pop(i)
-                            changed = True
-
-                            break
-
-                        if len(clause1) == 1 and len(clause2) != 1:
+                        # Simplify clauses depending on the length and conflict type
+                        if isinstance(lit1, list) or (len(clause1) == 1 and isinstance(lit1, Literal)):
+                            cnf_set.pop(max(i, j))
+                            cnf_set.pop(min(i, j))
+                        elif len(clause1) == 1:
                             cnf_set.pop(i)
                             clause2.remove(lit2)
-                            changed = True
-                            break
-                        if len(clause2) == 1 and len(clause1) != 1:
+                        elif len(clause2) == 1:
                             cnf_set.pop(j)
                             clause1.remove(lit1)
-                            changed = True
-                            break
-
-                        clause1.remove(lit1)
-                        clause2.remove(lit2)
+                        else:
+                            clause1.remove(lit1)
+                            clause2.remove(lit2)
 
                         changed = True
                         break
 
-                    changed = False
                     j += 1
 
-                    if changed:
-                        break
+                if changed:
+                    break
+                i += 1
 
-                if not (changed):
-                    i += 1
-
-            if cnf_set:
-                return False
-
-            return True
+        return len(cnf_set) == 0
 
     def __str__(self):
         return f'({self.formula})'
