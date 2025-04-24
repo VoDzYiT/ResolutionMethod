ae9561f831f005d1cb6b5ab20158161abad4e257
Volodymyr Zhytelnyi <volodymyr.zhytelnyi@gmail.com>
2025-04-24 20:48:54 +0300

    Improve the explanation

--- Main.py
+++ Main.py
@@ -8,8 +8,9 @@ B = Literal("Play")
 # After we need to connect them using implication
 implication = Implication(A, B)
 
-# Now we need to add knowledge and negation of goal
-formula = And(Or(Not(A), B), A, Not(B))
+# Now we need to add the knowledge base and the negation of the goal
+# I’m using And to combine them, since the whole formula should be in CNF
+formula = And(implication, A, Not(B))
 
 # Now we apply resolution method to check if contradiction appears
 # If we get True (an empty clause), it means our conclusion "Play" is logically valid
